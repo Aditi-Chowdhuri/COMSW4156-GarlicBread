8febb2f97d1a054a3ceece24ce6b9543af8c6753
Abhishek Paul <paul.abhishek2000@gmail.com>
2024-10-13 18:02:07 -0400

    fixed some entity relationships

--- src/main/java/com/garlicbread/includify/entity/resource/Resource.java
+++ src/main/java/com/garlicbread/includify/entity/resource/Resource.java
@@ -1,5 +1,6 @@
 package com.garlicbread.includify.entity.resource;
 
+import com.garlicbread.includify.entity.appointment.Appointment;
 import com.garlicbread.includify.entity.organisation.Organisation;
 import com.garlicbread.includify.entity.user.UserCategory;
 import jakarta.persistence.*;
@@ -25,10 +26,20 @@ public class Resource {
     @JoinColumn(name = "organisation_id", nullable = false)
     private Organisation organisation;
 
-    @ManyToMany(mappedBy = "resource", fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "resource_types",
+        joinColumns = @JoinColumn(name = "resource_id"),
+        inverseJoinColumns = @JoinColumn(name = "type_id")
+    )
     private List<ResourceType> resourceType;
 
-    @ManyToMany(mappedBy = "resource", fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "resource_user_categories",
+        joinColumns = @JoinColumn(name = "resource_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private List<UserCategory> targetUserCategory;
 
     @Column(nullable = false)
@@ -37,5 +48,8 @@ public class Resource {
     private String description;
 
     private String usageInstructions;
+
+    @ManyToMany(mappedBy = "resources")
+    private List<Appointment> appointments;
 }
 
