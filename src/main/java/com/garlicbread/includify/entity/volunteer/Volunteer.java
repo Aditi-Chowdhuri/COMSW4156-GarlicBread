24405be7e349c2116fec328774684922ab33a68c
Rahaf Bin Muhanna <rahafbinmuhanna@Rahafs-MacBook-Pro.local>
2024-10-18 21:40:08 -0400

    setters added

--- src/main/java/com/garlicbread/includify/entity/volunteer/Volunteer.java
+++ src/main/java/com/garlicbread/includify/entity/volunteer/Volunteer.java
@@ -49,6 +49,7 @@ public class Volunteer {
   @NotBlank(message = "Phone number is required")
   private String phone;
 
+  // Getters
   public String getEmail() {
     return email;
   }
@@ -81,4 +82,24 @@ public class Volunteer {
     return phone;
   }
 
+  // Setters
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 }
